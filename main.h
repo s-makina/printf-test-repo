75e588265cd6a8a0cad08702a6510a2156dce519
Salvation Makina <salvationemakina3@gmail.com>
2022-10-18 18:51:01 +0200

    main

--- main.h
+++ main.h
@@ -48,16 +48,16 @@ int print_hexadecimal(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_hexa_upper(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
-
 int print_hexa(va_list types, char map_to[],
 char buffer[], int flags, char flag_ch, int width, int precision, int size);
-
 int print_non_printable(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
-
 int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
-
+int print_reverse(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_rot13string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
 /**
  * struct fmt - Struct op
  *
