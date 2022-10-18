1754a85d3b7a73857cba827760e27b5f45c90642
Salvation Makina <salvationemakina3@gmail.com>
2022-10-18 18:31:44 +0200

    specifiers

--- main.h
+++ main.h
@@ -1,7 +1,14 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+#define BUFF_SIZE 1024
 
 #include <stdio.h>
+#include <stdarg.h>
 
 void print_buffer(char buffer[], int *buff_ind);
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
+
 #endif
