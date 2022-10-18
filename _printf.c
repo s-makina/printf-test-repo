dd90aa29874cd5d50b710c3943da5ad3bf015089
Salvation Makina <salvationemakina3@gmail.com>
2022-10-18 18:36:38 +0200

    Flags

--- _printf.c
+++ _printf.c
@@ -35,7 +35,7 @@ int _printf(const char *format, ...)
 			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
-			printed = handle_print(format, &i, list, buffer,
+			printed = print_handler(format, &i, list, buffer,
 				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
--- main.h
+++ main.h
@@ -2,10 +2,20 @@
 #define _MAIN_H_
 #define BUFF_SIZE 1024
 
+/* PRINT FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
 #include <stdio.h>
 #include <stdarg.h>
 
 void print_buffer(char buffer[], int *buff_ind);
+int print_handler(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
+
 int get_flags(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
